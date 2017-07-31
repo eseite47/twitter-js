2cafdb8cb0588a687eddc88b9d9be2ec5fad5b9a
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-07-31 18:02:00 -0400

    added volley

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const volley = require('volleyball')
 const app = express();
+
+// app.use(function(req, res, next){
+//   console.log("Request: "+ req.method +" "+ req.url +"\n" + res.statusCode);
+//   next();
+// })
+app.use(volley);
 
 app.get("/", function(req, res){
   res.send("Ellie and Boyan are trying this out.");
