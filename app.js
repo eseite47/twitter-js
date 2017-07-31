1884eca275e50b5d380fd5f26fad3c0756316b63
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-07-31 18:26:05 -0400

    rendering nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
-const volley = require('volleyball')
+const volley = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express();
 
-// app.use(function(req, res, next){
-//   console.log("Request: "+ req.method +" "+ req.url +"\n" + res.statusCode);
-//   next();
-// })
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
 app.use(volley);
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+//var test = nunjucks.render('index.html', locals);
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
 app.get("/", function(req, res){
-  res.send("Ellie and Boyan are trying this out.");
+  //res.send(test);
+  res.render( 'index', {title: 'Hall of Fame', people: people} )
+})
+
+app.get("/locals", function(req, res){
+  //res.send(test);
+  //res.render( 'index', {title: 'Hall of Fame', people: people} )
+  res.render('index', locals);
 })
 
 app.listen('3000', function(){
