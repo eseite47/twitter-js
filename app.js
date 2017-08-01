fff48b143c89271a070bea20d0bd220470e16dc4
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-08-01 12:06:26 -0400

    adding form

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const volley = require('volleyball');
 const nunjucks = require('nunjucks');
 const tweetBank = require('./tweetBank');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(volley);
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
 router.get( '/users/:name', function(req, res){
@@ -16,8 +16,8 @@ router.get( '/users/:name', function(req, res){
   //let tweets = tweetBank.list();
   //let tweet = tweets.find(function(usr){ usr.name === user })
   let tweet = tweetBank.find({'name': user});
-  console.log(tweet)
-  console.log(req.params.name);
+  //console.log(tweet)
+  //console.log(req.params.name);
   res.render( 'index', { tweets: tweet } );
 } );
 
@@ -26,9 +26,17 @@ router.get( '/user/:id', function(req, res){
   //let tweets = tweetBank.list();
   //let tweet = tweets.find(function(usr){ usr.name === user })
   let tweet = tweetBank.find({'id': id});
-  console.log(tweet)
-  console.log(req.params.name);
+  //console.log(tweet)
+  //console.log(req.params.name);
   res.render( 'index', { tweets: tweet } );
 } );
+
+router.post('/tweets', function(req, res) {
+  console.log(req.body);
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 module.exports = router;
